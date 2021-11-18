2eccf864c08bf0f2d2382c592f13349a8d8fb5de
xiaowei <1744114980@qq.com>
2021-11-18 14:58:53 +0800

    hot-fix1 commit

--- src/main/java/com/xw/git/GitTest.java
+++ src/main/java/com/xw/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 222222");
+        System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
